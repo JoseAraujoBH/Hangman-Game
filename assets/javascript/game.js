d7179c668449c1b5c7d721e3b2407752cb735475
Jose Araujo <JoseAraujoBH@gmail.com>
2018-03-02 12:55:36 -0500

    added more code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,12 @@ const word = ['Mickey Mouse', 'Minnie Mouse', 'Donald Duck', 'Daisy Duck', 'Scro
 
 let randNum = Math.floor(Math.random() * word.length);
 let chosenWord = word[randNum];
-console.log(chosenWord);
+let rightWord = [];
+let wrongWord = [];
 let underScore = [];
+
+// console.log(chosenWord);
+
 
 
 // Create underscores based on length of word
@@ -19,23 +23,31 @@ let generateUnderscore = () => {
     return underScore;
 }
 
-console.log(generateUnderscore());
+// console.log(generateUnderscore());
 
 // Get users guess 
 
 document.addEventListener('keypress', (event) => {
-    console.log(event);
+    // console.log(event);
     let keyword = String.fromCharCode(event.keyCode);
-    console.log(keyword);
+    // console.log(keyword);
 
     // let keycode = event.keyCode;
     // console.log(keycode);
     // let keyWord = String.fromCharCode(keycode);
     // console.log(keyWord);
 
-    console.log(chosenWord.indexOf(keyword));
-    if(keyword.indexOf(chosenWord) > -1) {
-        console.log(true);
+    // console.log(chosenWord.indexOf(keyword));
+    if(chosenWord.indexOf(keyword) > -1) {
+        // console.log(true);
+    rightWord.push(keyord);
+    // console.log(rightWord);
+    underScore[chosenWord.indexOf(keyword)] = keyword;
+    if(underScore.join('') == chosenWord) {
+        alert('You Win');
+    }
+    wrongWord.push(keyword);
+    // console.log(wrongWord);
     }
 
 });
